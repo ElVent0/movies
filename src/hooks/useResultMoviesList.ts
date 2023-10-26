824ff1aa0e25c189b2451816b65faa2a6744cc1a
Vas <vasylviter95@gmail.com>
2023-10-26 22:04:48 +0300

    Fix
    Fix: fix Object Calisthenics

--- src/hooks/useResultMoviesList.ts
+++ src/hooks/useResultMoviesList.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+// import { BsExclamationSquareFill } from "react-icons/bs";
 import { MovieType } from "../movieType";
 
 export const useResultMoviesList = (
@@ -11,23 +12,25 @@ export const useResultMoviesList = (
   const [genresList] = useState<string[]>([
     ...new Set(movies.map((item) => item.category)),
   ]);
+  console.log(movies);
   useEffect(() => {
-    const resultMovies = movies
-      .filter((item) => {
-        return item.title
-          .toLocaleLowerCase()
-          .includes(inputData.toLocaleLowerCase());
-      })
-      .filter((item) =>
-        ratingToggles.length && !ratingToggles.includes("any")
-          ? ratingToggles.includes(item.rating.split(".")[0])
-          : item
-      )
-      .filter((item) =>
-        genreToggles.length && !genreToggles.includes("any")
-          ? genreToggles.includes(item.category)
-          : item
-      );
+    const resultMovies = movies.filter((movie) => {
+      const filteredByTitle = movie.title
+        .toLowerCase()
+        .includes(inputData.toLocaleLowerCase());
+
+      const filteredByRating =
+        ratingToggles.length && ratingToggles.includes("any")
+          ? true
+          : ratingToggles.includes(String(Math.floor(Number(movie.rating))));
+
+      const filteredByGenre =
+        genreToggles.length && genreToggles.includes("any")
+          ? true
+          : genreToggles.includes(movie.category);
+
+      return filteredByTitle && filteredByRating && filteredByGenre && movie;
+    });
 
     setResultMoviesList(resultMovies);
   }, [genreToggles, inputData, movies, ratingToggles]);
